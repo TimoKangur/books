f2ae28e42dc786ac7d50b55de03debf797a8f493
TimoKangur <92432796+TimoKangur@users.noreply.github.com>
2022-02-08 19:40:02 +0200

    Add files via upload

--- classes/LS.js
+++ classes/LS.js
@@ -1,33 +1,31 @@
-class LS {
-    getData(key) {
+class LS{
+    getData(key){
         let data
-        if (localStorage.getItem('key') === null) {
+        if(localStorage.getItem(key) === null){
             data = []
         } else {
-            data = JSON.parse(localStorage.getItem('key'))
+            data = JSON.parse(localStorage.getItem(key))
         }
         return data
     }
 
-    setData(key, data) {
+    setData(key, data){
         localStorage.setItem(key, JSON.stringify(data))
     }
 
-    addBook(book) {
-        const books = this.getData("books")
+    addBook(book){
+        const books = this.getData('books')
         books.push(book)
-        this.setData("books", books)
+        this.setData('books', books)
     }
 
     delBook(book){
-        let books = this.getData("books")
-        console.log(books)
-        books.forEach(function (bookFromLS, bookIndex){
-            if(bookFromLS.title === book.title && bookFromLS.author && bookFromLS.isbn === book.isbn) {
-                book.splice(bookIndex, 1)
+        let books = this.getData('books')
+        books.forEach(function(bookFromLS, bookIndex){
+            if(bookFromLS.title === book.title && bookFromLS.author === book.author && bookFromLS.isbn === book.isbn){
+                books.splice(bookIndex, 1)
             }
-            })
-            this.setData("books", books)
-
+        })
+        this.setData('books', books)
     }
 }
